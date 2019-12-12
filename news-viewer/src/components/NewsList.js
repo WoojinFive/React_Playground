e188d1b385620e9e48293979016b92d5e3cf0028
WooJin Oh <woojin@WooJinui-iMac.local>
2019-12-12 00:22:18 -0400

    news viewer app

---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import usePromise from '../lib/usePromise';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=ca${query}&apiKey=12b1f0c849034f3cb7763dba31f9fd76`,
+    );
+  }, [category]);
+
+  if (loading) {
+    return <NewsListBlock>Waiting...</NewsListBlock>;
+  }
+  if (!response) {
+    return null;
+  }
+  if (error) {
+    return <NewsListBlock>Error!</NewsListBlock>;
+  }
+  // if response Okay
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map(article => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
