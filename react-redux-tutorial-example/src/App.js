9c9ce2dfcf05a6abc47ea8d845d77b7841b61b2b
kh3996 <kh3996@gmail.com>
2020-01-20 00:18:22 -0400

    with out redux example

--- react-redux-tutorial-example/src/App.js
+++ react-redux-tutorial-example/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends Component {
+  state = { number: 0 }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot onClick={function(size){
+          this.setState({number: this.state.number + size})
+        }.bind(this)}></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+      </div>
+    );
+  } 
 }
 
 export default App;
