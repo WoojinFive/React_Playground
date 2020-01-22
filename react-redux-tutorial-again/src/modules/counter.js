ff7f941f6a8b39203975c8523cca2f1c39f48467
kh3996 <kh3996@gmail.com>
2020-01-21 23:34:06 -0400

    using redux-actions

--- react-redux-tutorial-again/src/modules/counter.js
+++ react-redux-tutorial-again/src/modules/counter.js
@@ -1,10 +1,15 @@
+import { createAction, handleActions } from 'redux-actions';
 //action type
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 //action creator
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+//using redux-actions
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
 /*
 export const decrease = () => {
   return {
@@ -19,19 +24,28 @@ const initialState = {
 };
 
 //reducer
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1
-      };
-    default:
-      return state;
-  }
-}
+// using redux-actions
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 export default counter;
--- react-redux-tutorial-again/src/modules/todos.js
+++ react-redux-tutorial-again/src/modules/todos.js
@@ -1,3 +1,5 @@
+import { createAction, handleActions } from 'redux-actions';
+
 //action type
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -5,31 +7,45 @@ const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
 //action creator
-export const changeInput = input => ({
-  type: CHANGE_INPUT,
-  input
-});
+//using redux-actions
+export const changeInput = createAction(CHANGE_INPUT, input => input);
 
 let id = 3;
 
-export const insert = text => ({
-  type: INSERT,
-  todo: {
-    id: id++,
-    text,
-    done: false
-  }
-});
+export const insert = createAction(INSERT, text => ({
+  id: id++,
+  text,
+  done: false,
+}));
 
-export const toggle = id => ({
-  type: TOGGLE,
-  id
-});
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
-export const remove = id => ({
-  type: REMOVE,
-  id
-});
+// export const changeInput = input => ({
+//   type: CHANGE_INPUT,
+//   input
+// });
+
+// let id = 3;
+
+// export const insert = text => ({
+//   type: INSERT,
+//   todo: {
+//     id: id++,
+//     text,
+//     done: false
+//   }
+// });
+
+// export const toggle = id => ({
+//   type: TOGGLE,
+//   id
+// });
+
+// export const remove = id => ({
+//   type: REMOVE,
+//   id
+// });
 
 //initial state
 const initialState = {
@@ -49,34 +65,56 @@ const initialState = {
 };
 
 //reducer
-function todos(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo)
-      };
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map(todo =>
-          todo.id === action.id ? { ...todo, done: !todo.done} : todo
-        )
-      };
+//using redux-actions
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map(todo => 
+        todo.id === id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter(todo => todo.id !== id),
+    }),
+  },
+  initialState,
+)
 
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.id !== action.id)
-      };
-    default:
-      return state;
-  }
-}
+// function todos(state = initialState, action) {
+//   switch (action.type) {
+//     case CHANGE_INPUT:
+//       return {
+//         ...state,
+//         input: action.input
+//       };
+//     case INSERT:
+//       return {
+//         ...state,
+//         todos: state.todos.concat(action.todo)
+//       };
+//     case TOGGLE:
+//       return {
+//         ...state,
+//         todos: state.todos.map(todo =>
+//           todo.id === action.id ? { ...todo, done: !todo.done} : todo
+//         )
+//       };
+
+//     case REMOVE:
+//       return {
+//         ...state,
+//         todos: state.todos.filter(todo => todo.id !== action.id)
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 export default todos;
