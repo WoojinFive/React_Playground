2b5f8335d277165278babe90801d1476d88a66e2
kh3996 <kh3996@gmail.com>
2020-03-23 22:44:15 -0300

    apply routing

--- complete-guide/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ complete-guide/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -97,30 +97,23 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandeler = () => {
     // alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Woojin Oh',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: 'B3N2H2',
-          country: 'Canada'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    };
 
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+
+    queryParams.push('price=' + this.state.totalPrice);
+
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   };
 
   render() {
