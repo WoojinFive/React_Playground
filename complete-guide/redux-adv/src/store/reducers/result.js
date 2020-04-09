2c24ade9b2476f1123f1e3b2fa967017bb31d6af
kh3996 <kh3996@gmail.com>
2020-04-08 22:52:57 -0300

    advanced redux tutorial

---
+++ complete-guide/redux-adv/src/store/reducers/result.js
@@ -0,0 +1,47 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
+const initialState = {
+  results: [],
+};
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (result) => result.id !== action.resultElId
+  );
+  return updateObject(state, {
+    results: updatedArray,
+  });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return updateObject(state, {
+        results: state.results.concat({
+          id: new Date(),
+          value: action.result * 2,
+        }),
+      });
+    // return {
+    //   ...state,
+    //   results: state.results.concat({
+    //     id: new Date(),
+    //     value: action.result * 2,
+    //   }),
+    // };
+    case actionTypes.DELETE_RESULT:
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1)
+      return deleteResult(state, action);
+    // return {
+    //   ...state,
+    //   results: updatedArray,
+    // };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
